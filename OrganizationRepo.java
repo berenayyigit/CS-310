4df9816da7297cce5c9417a91d2bad0f1ae2cbe9
Berenay Yiğit <ebrarberenayyigit@gmail.com>
2024-04-27 12:50:00 +0300

    Organization part added
    Organization.java, OrganizationRepo.java, EventService.java created

---
+++ OrganizationRepo.java
@@ -0,0 +1,6 @@
+package com.example.demo.repo;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import com.example.demo.model.Club;
+
+public interface OrganizationRepo extends MongoRepository
